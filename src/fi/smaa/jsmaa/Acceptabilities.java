ee0cda49f67d89ddce4db7f0cec242b86562bdf6
tommite <t.p.tervonen@rug.nl>
2009-07-16 12:35:29 +0200

    Refactored to have separate class for acceptability-results

---
+++ src/fi/smaa/jsmaa/Acceptabilities.java
@@ -0,0 +1,75 @@
+/*
+	This file is part of JSMAA.
+	(c) Tommi Tervonen, 2009	
+
+    JSMAA is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    JSMAA is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with JSMAA.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package fi.smaa.jsmaa;
+
+import java.util.List;
+
+import fi.smaa.jsmaa.model.Alternative;
+
+public class Acceptabilities extends ResultsMap {
+	
+	protected int[][] hits;	
+
+	public Acceptabilities(List<Alternative> alternatives, int size) {
+		super(alternatives, size);
+		hits = new int[alternatives.size()][size];		
+	}
+
+	public void computeResults() {
+		calculateAcceptabilities();
+	}
+		
+	private void calculateAcceptabilities() {
+		for (Integer altIndex : results.keySet()) {
+			List<Double> vec = results.get(altIndex);
+			for (int i=0;i<vec.size();i++) {
+				vec.set(i, calculateAcceptability(altIndex, i));
+			}
+		}
+	}
+
+	private double calculateAcceptability(Integer altIndex, int rank) {
+		int totalIter = 0;
+		for (int i=0;i<hits[altIndex].length;i++) {
+			totalIter += hits[altIndex][i];
+		}
+		return (double) hits[altIndex][rank] / (double) totalIter;
+	}	
+	
+	public void rankHit(int altIndex, int resIndex) {
+		hits[altIndex][resIndex]++;
+	}	
+	
+	
+	public int getTotalHits(int altIndex) {
+		int total = 0;
+		if (hits.length < 1) {
+			return 0;
+		}
+		for (int i=0;i<hits[0].length;i++) {
+			total += hits[0][i];
+		}
+		return total;
+	}	
+	
+	public int getHits(int altIndex, int resIndex) {
+		return hits[altIndex][resIndex];
+	}	
+
+}
---
+++ src/fi/smaa/jsmaa/SMAAResults.java
@@ -0,0 +1,65 @@
+/*
+	This file is part of JSMAA.
+	(c) Tommi Tervonen, 2009	
+
+    JSMAA is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    JSMAA is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with JSMAA.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package fi.smaa.jsmaa;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import fi.smaa.jsmaa.model.Alternative;
+
+public abstract class SMAAResults {
+
+	protected List<SMAAResultsListener> listeners = new ArrayList<SMAAResultsListener>();
+	protected List<Alternative> alternatives;
+	
+	protected SMAAResults(List<Alternative> alts) {
+		this.alternatives = alts;
+	}
+
+	public void addResultsListener(SMAAResultsListener listener) {
+		if (!listeners.contains(listener)) {
+			listeners.add(listener);
+		}
+	}
+
+	public void removeResultsListener(SMAAResultsListener listener) {
+		listeners.remove(listener);
+	}
+
+	protected void fireResultsChanged() {
+		for (SMAAResultsListener listener : listeners) {
+			listener.resultsChanged();
+		}
+	}
+
+	public List<Alternative> getAlternatives() {
+		return alternatives;
+	}
+
+	protected Map<Alternative, List<Double>> transformMap(Map<Integer, List<Double>> map) {
+		Map<Alternative, List<Double>> cw = new HashMap<Alternative, List<Double>>();		
+		for (Integer index : map.keySet()) {
+			cw.put(alternatives.get(index), map.get(index));
+		}
+		return cw;
+	}
+	
+}
