fea4a27a2db2978b0dc1d0e8bc4696fc3b4c44a7
Tommi Tervonen <t.p.tervonen@rug.nl>
2010-05-06 16:39:13 +0200

    fixing

--- lib2/src/main/java/fi/smaa/jsmaa/model/IterationException.java
+++
@@ -1,8 +0,0 @@
-package fi.smaa.jsmaa.model;
-
-@SuppressWarnings("serial")
-public class IterationException extends Exception {
-	public IterationException(String message) {
-		super(message);
-	}
-}
