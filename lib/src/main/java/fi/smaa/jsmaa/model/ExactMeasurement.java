13711151c9980040dd7a6f7d80c2fd65cf647158
Tommi Tervonen <tervonen@ese.eur.nl>
2012-01-22 22:05:36 +0100

    Add toString to ExactMeasurement

--- lib/src/main/java/fi/smaa/jsmaa/model/ExactMeasurement.java
+++ lib/src/main/java/fi/smaa/jsmaa/model/ExactMeasurement.java
@@ -49,6 +49,11 @@ public final class ExactMeasurement extends CardinalMeasurement {
 	public Interval getRange() {
 		return new Interval(val, val);
 	}
+	
+	@Override
+	public String toString() {
+		return "[" + val + "]";
+	}
 
 	@Override
 	public double sample() {
