258889b8cab5cbaa312f3035f7e743aa0c8df70b
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2012-05-09 15:31:00 +0200

    Work around for bug in CorrelatedRandomVectorGenerator (issue #493)

--- lib/src/main/java/fi/smaa/common/MultivariateGaussianGenerator.java
+++ lib/src/main/java/fi/smaa/common/MultivariateGaussianGenerator.java
@@ -1,5 +1,7 @@
 package fi.smaa.common;
 
+import java.util.Arrays;
+
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
 import org.apache.commons.math3.random.CorrelatedRandomVectorGenerator;
@@ -10,20 +12,46 @@ import org.apache.commons.math3.random.RandomVectorGenerator;
 public class MultivariateGaussianGenerator implements RandomVectorGenerator {
 	private CorrelatedRandomVectorGenerator generator;
 	private double[] mean;
+	private int[] rowMap;
 	
 	public MultivariateGaussianGenerator(RealVector mean, RealMatrix covariance, RandomGenerator generator) {
 		this.mean = mean.toArray();
+		
 		if (covariance.getNorm() != 0) {
-			this.generator = new CorrelatedRandomVectorGenerator(mean.toArray(), covariance,
-					1.0e-12 * covariance.getNorm(), new GaussianRandomGenerator(generator));
+			final int n = covariance.getColumnDimension();
+			final double small = 1.0e-12 * covariance.getNorm();
+			rowMap = new int[n];
+			int[] select = new int[n]; 
+					
+			int j = 0;
+			for (int i = 0; i < n; ++i) {
+				if (covariance.getEntry(i, i) < small) {
+					rowMap[i] = -1;
+				} else {
+					rowMap[i] = j;
+					select[j] = i;
+					++j;
+				}
+			}
+			final int m = j;
+			int[] selectTruncated = Arrays.copyOf(select, m);
+			covariance = covariance.getSubMatrix(selectTruncated, selectTruncated);
+			
+			this.generator = new CorrelatedRandomVectorGenerator(new double[m], covariance,
+					small, new GaussianRandomGenerator(generator));
 		}
 	}
 
 	@Override
 	public double[] nextVector() {
-		if (this.generator == null) {
-			return this.mean;
+		double sample[] = new double[this.mean.length];
+		System.arraycopy(this.mean, 0, sample, 0, sample.length);
+		if (this.generator != null) {
+			double[] x = this.generator.nextVector();
+			for (int i = 0; i < mean.length; ++i) {
+				sample[i] += rowMap[i] < 0 ? 0.0 : x[rowMap[i]];
+			}
 		}
-		return this.generator.nextVector();
+		return sample;
 	}
 }
--- lib/src/test/java/fi/smaa/common/MultivariateGaussianGeneratorTest.java
+++ lib/src/test/java/fi/smaa/common/MultivariateGaussianGeneratorTest.java
@@ -2,6 +2,7 @@ package fi.smaa.common;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import junit.framework.AssertionFailedError;
 
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.ArrayRealVector;
@@ -9,9 +10,13 @@ import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
 import org.apache.commons.math3.random.JDKRandomGenerator;
 import org.apache.commons.math3.random.RandomGenerator;
+import org.apache.commons.math3.stat.correlation.StorelessCovariance;
 import org.junit.Test;
 
 public class MultivariateGaussianGeneratorTest {
+	private static final double EPSILON = 0.001;
+	private static final int N_SAMPLES = 10000;
+
 	@Test
 	public void testSimpleWrap() {
 		RealVector mean = new ArrayRealVector(new double[] { 1.0, 2.0, 3.0 });
@@ -42,9 +47,65 @@ public class MultivariateGaussianGeneratorTest {
 	@Test
 	public void testFullCovariance() {
 		RealVector mean = new ArrayRealVector(new double[] { 1.0, 2.0, 3.0 });
-		RealMatrix covariance = new Array2DRowRealMatrix(new double[][] { {1.0, 1.0, 0.0}, {1.0, 1.0, 0.0}, {0.0, 1.0, 0.0} });
+		RealMatrix covariance = new Array2DRowRealMatrix(new double[][] { {1.0, 1.0, 0.0}, {1.0, 1.0, 0.0}, {0.0, 0.0, 1.0} });
 		RandomGenerator generator = new JDKRandomGenerator();
 		double[] nextVector = new MultivariateGaussianGenerator(mean, covariance, generator).nextVector();
 		assertEquals(nextVector[0] - 1.0, nextVector[1] - 2.0, 10e-9);
+	}
+		
+	/**
+	 * There is a bug in apache commons-math3 - it does not always decompose the covariance matrix correctly.
+	 * @see <a href="https://issues.apache.org/jira/browse/MATH-789">bug MATH-789</a>
+	 */
+	@Test
+	public void testWorkAroundForCommonsBug() {
+		final double[][] covMatrix1 = new double[][]{
+				{0.013445532, 0.010394690, 0.009881156, 0.010499559},
+				{0.010394690, 0.023006616, 0.008196856, 0.010732709},
+				{0.009881156, 0.008196856, 0.019023866, 0.009210099},
+				{0.010499559, 0.010732709, 0.009210099, 0.019107243}
+		};
+		
+		final double[][] covMatrix2 = new double[][]{
+				{0.0, 0.0, 0.0, 0.0, 0.0},
+				{0.0, 0.013445532, 0.010394690, 0.009881156, 0.010499559},
+				{0.0, 0.010394690, 0.023006616, 0.008196856, 0.010732709},
+				{0.0, 0.009881156, 0.008196856, 0.019023866, 0.009210099},
+				{0.0, 0.010499559, 0.010732709, 0.009210099, 0.019107243}
+		};
+		
+		final double[][] covMatrix3 = new double[][]{
+				{0.013445532, 0.010394690, 0.0, 0.009881156, 0.010499559},
+				{0.010394690, 0.023006616, 0.0, 0.008196856, 0.010732709},
+				{0.0, 0.0, 0.0, 0.0, 0.0},
+				{0.009881156, 0.008196856, 0.0, 0.019023866, 0.009210099},
+				{0.010499559, 0.010732709, 0.0, 0.009210099, 0.019107243}
+		};
+		
+		testSampler(covMatrix1);
+		testSampler(covMatrix2);
+		testSampler(covMatrix3);
+	}
+
+	private void testSampler(final double[][] covMatrix) {
+		MultivariateGaussianGenerator sampler = new MultivariateGaussianGenerator(
+				new ArrayRealVector(covMatrix.length),
+				new Array2DRowRealMatrix(covMatrix),
+				new JDKRandomGenerator());
+		
+		StorelessCovariance cov = new StorelessCovariance(covMatrix.length);
+		for (int i = 0; i < N_SAMPLES; ++i) {
+			double[] sample = sampler.nextVector();
+			cov.increment(sample);
+		}
+
+		double[][] sampleCov = cov.getData();
+		for (int r = 0; r < covMatrix.length; ++r) {
+			try {
+				assertArrayEquals(covMatrix[r], sampleCov[r], EPSILON);
+			} catch (Error e) {
+				throw new AssertionFailedError("Matrices differ at row " + r + ": " + e.getMessage());
+			}
+		}
 	}
 }
